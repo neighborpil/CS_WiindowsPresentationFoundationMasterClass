5243a13c64479a079a61a8f06d97732ed2681ec8
neighborpil <neighborpil@naver.com>
2019-04-18 17:57:17 +0900

    sql practice

---
+++ CH06_SqlLightTheContactApp/Lec34_DesktopContactApp/Classes/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Lec34_DesktopContactApp.Classes
+{
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        //[MaxLength(50)]
+        //[Indexed]
+        //[Unique]
+
+        //[Ignore]
+        //public string FullName { get; }
+
+        //[Ignore]
+        //public List<Contact> Inhabitats { get; set; }
+    }
+}
---
+++ CH06_SqlLightTheContactApp/Lec34_DesktopContactApp/NewContactWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Lec34_DesktopContactApp.Classes;
+using SQLite;
+
+namespace Lec34_DesktopContactApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //Todo: Save contact
+            Contact contact = new Contact()
+            {
+                Name = NameTextBox.Text,
+                Email = EmailTextBox.Text,
+                Phone = PhoneNumberTextBox.Text
+            };
+
+            string databaseName = "Contacts.db";
+            string forderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(forderPath, databaseName);
+
+            SQLiteConnection connection = new SQLiteConnection(databasePath);
+            connection.CreateTable<Contact>();
+            connection.Insert(contact);
+            connection.Close();
+
+            this.Close();
+        }
+    }
+}
